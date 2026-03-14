1bb2a4aa8ec4543bcdf29a1ea019693387596fb3
apurbochaki-lab <apurbokumarchaki@gmail.com>
2026-03-14 21:34:30 +0600

    My project the Nation Find is ready to build

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countries from './Components/Countries/Countries'
+
+function App() {
+
+  return (
+    <>
+      <h1 style={{textAlign: 'center'}}>React Countries Project</h1>
+
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
